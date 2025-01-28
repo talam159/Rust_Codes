2ca52135c8155ecd93c82517314acf7aedc8b016
tanvirul <malam152159@bscse.uiu.ac.bd>
2025-01-28 17:52:24 +0100

    valid parentheses solved

---
+++ src/leetcode/valid_parentheses.rs
@@ -0,0 +1,56 @@
+use std::io;
+struct Solution;
+
+impl Solution{
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = Vec::new();
+        
+        for c in s.chars(){
+            if c =='{' || c=='(' || c=='['{
+                stack.push(c);
+            }
+
+            if c =='}' || c==')' || c==']'{
+                
+
+            }
+            
+            if c=='}'{
+                if stack.last() ==Some(&'{'){
+                    stack.pop();
+                }
+                else{
+                return false;
+                }
+            } 
+            if c==')'{
+                if stack.last() ==Some(&'('){
+                    stack.pop();
+                }
+                else{
+                return false;
+                }
+            }
+            if c==']'{
+                if stack.last() ==Some(&'['){
+                    stack.pop();
+                }
+                else{
+                return false;
+                }
+            }
+        }
+
+        if stack.is_empty(){
+            true
+        }else{
+            false
+        }
+    }
+}
+
+fn main(){
+    let par = "()";
+    let mut result = is_valid(par);
+    println!("{}", result);
+}
